71e027815e7c0c44244a988660f65d8ec88afd03
George Colon <geroge_colon@oneilluminate.com>
2024-08-22 09:39:02 -0700

    mongoDB working on Client and express node next add AtlasDB

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/todo-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Todo Schema
+const todoSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+// Routes
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post('/todos', async (req, res) => {
+  const newTodo = new Todo({
+    title: req.body.title,
+    completed: false,
+  });
+  await newTodo.save();
+  res.json(newTodo);
+});
+
+app.put('/todos/:id', async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.completed = req.body.completed;
+  await todo.save();
+  res.json(todo);
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+  res.json(result);
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
